47ea7958cb028042b5e1c59d11eb7f375291a650
gautaz <11046232+gautaz@users.noreply.github.com>
2022-11-30 16:10:25 +0100

    build & test ISO image

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "Flake for building a custom NixOS installation media image";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.05";
+  };
+  outputs = { self, nixpkgs }: {
+    nixosConfigurations = {
+      installMedia = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          {
+            imports = [ ./modules ];
+          }
+	  "${nixpkgs}/nixos/modules/installer/cd-dvd/channel.nix"
+        ];
+      };
+    };
+  };
+}
