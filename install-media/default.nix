e0f06bc9f6e305e17eb31eb051f1c415d32ea949
gautaz <11046232+gautaz@users.noreply.github.com>
2022-12-28 22:47:43 +0100

    git is needed by nixos-install --flake

--- install-media/default.nix
+++ install-media/default.nix
@@ -1,9 +1,13 @@
-{ ... }:
+{ pkgs, ... }:
+with pkgs;
 {
   imports = [
     ./configuration
     ./host
     ./iso.nix
     ./storage
+  ];
+  environment.systemPackages = [
+    git # needed by nixos-install --flake
   ];
 }
