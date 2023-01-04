658d02d92e7e8277e914781f8ab82ca5d7735a46
gautaz <11046232+gautaz@users.noreply.github.com>
2023-01-04 21:16:40 +0100

    enable extended window manager hints

--- homes/del/xmonad.hs
+++ homes/del/xmonad.hs
@@ -1,10 +1,12 @@
 import XMonad (def, mod4Mask, modMask, spawn, terminal, xmonad)
+import XMonad.Hooks.EwmhDesktops (ewmh, ewmhFullscreen)
 import XMonad.Hooks.StatusBar (defToggleStrutsKey, statusBarProp, withEasySB)
 import XMonad.Hooks.StatusBar.PP (xmobarPP)
 import XMonad.Util.EZConfig (additionalKeysP)
 
 main :: IO ()
-main = xmonad . withEasySB mySB defToggleStrutsKey $ myConfig
+main =
+  xmonad . ewmhFullscreen . ewmh . withEasySB mySB defToggleStrutsKey $ myConfig
 
 mySB = statusBarProp "xmobar" (pure xmobarPP)
 
