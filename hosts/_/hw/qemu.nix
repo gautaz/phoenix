5eab58aae6db650d40301b16cd41fbc3b097a7ca
gautaz <11046232+gautaz@users.noreply.github.com>
2023-01-07 09:50:48 +0100

    cosmetics

--- hosts/_/hw/qemu.nix
+++ hosts/_/hw/qemu.nix
@@ -1,17 +1,15 @@
 {
   config,
   lib,
-  pkgs,
   modulesPath,
   ...
 }: {
   imports = [(modulesPath + "/profiles/qemu-guest.nix") ../../../modules/qemu-mount-host];
+  boot.extraModulePackages = [];
   boot.initrd.availableKernelModules = ["ata_piix" "virtio_pci" "floppy" "sr_mod" "virtio_blk"];
   boot.initrd.kernelModules = ["dm-snapshot"];
   boot.kernelModules = [];
-  boot.extraModulePackages = [];
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
   networking.useDHCP = lib.mkDefault true;
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.intel.updateMicrocode =
-    lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
