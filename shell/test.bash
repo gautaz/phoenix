ba2061362f20f62392c8a13617244eb72933b6bb
gautaz <11046232+gautaz@users.noreply.github.com>
2023-01-14 16:20:58 +0100

    kusanagi needs more disk space

--- shell/test.bash
+++ shell/test.bash
@@ -4,7 +4,7 @@ OVMF_PATH="$(nix-store -q --references "$(echo "$BUILD_INPUTS" | tr ' ' '\n' | g
 cd "$(git rev-parse --show-toplevel)"
 
 if [ ! -f disk.qcow2 ]; then
-	qemu-img create -f qcow2 disk.qcow2 10G
+	qemu-img create -f qcow2 disk.qcow2 20G
 fi
 
 qemu-system-x86_64 -enable-kvm -m 2048 -bios "$OVMF_PATH/FV/OVMF.fd" -cdrom result.iso/iso/nixos-*-x86_64-linux.iso -drive file=disk.qcow2,media=disk,if=virtio -fsdev local,path="$(pwd)",security_model=mapped-xattr,id=ninep,readonly=on -device virtio-9p-pci,fsdev=ninep,mount_tag=hostmnt -nic user,hostfwd=tcp::5511-:22
