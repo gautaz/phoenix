8dbe71f4610ff4c3b3fccb4ed0e705aa07e430b3
gautaz <11046232+gautaz@users.noreply.github.com>
2023-01-25 22:09:58 +0100

    improve bash history
    See https://sanctum.geek.nz/arabesque/better-bash-history/

--- homes/del/bash.nix
+++ homes/del/bash.nix
@@ -21,9 +21,21 @@ _: {
       }
     '';
     historyControl = ["ignoredups"];
+    historyIgnore = ["bg" "cd" "exit" "fg" "history" "ls"];
     initExtra = ''
       _completion_loader passage
       complete -o filenames -F _pass pass
     '';
+    sessionVariables = {
+      HISTTIMEFORMAT = "%F %T ";
+    };
+    shellOptions = [
+      "checkjobs"
+      "checkwinsize"
+      "cmdhist"
+      "extglob"
+      "globstar"
+      "histappend"
+    ];
   };
 }
