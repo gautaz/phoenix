35507f25122c2e4d9b505b02ccbec06450390711
gautaz <11046232+gautaz@users.noreply.github.com>
2023-01-28 11:36:36 +0100

    add statusline plugin (feline)

--- homes/del/neovim.nix
+++ homes/del/neovim.nix
@@ -4,7 +4,25 @@
     enable = true;
     plugins = with pkgs.vimPlugins; [
       {
-        config = "require'lspconfig'.nil_ls.setup{}";
+        config = ''
+          vim.opt.termguicolors = true
+          require('feline').setup{}
+        '';
+        plugin = feline-nvim;
+        type = "lua";
+      }
+      {
+        config = "require('gitsigns').setup{}";
+        plugin = gitsigns-nvim;
+        type = "lua";
+      }
+      {
+        config = "require('nvim-web-devicons').setup{}";
+        plugin = nvim-web-devicons;
+        type = "lua";
+      }
+      {
+        config = "require('lspconfig').nil_ls.setup{}";
         plugin = nvim-lspconfig;
         type = "lua";
       }
