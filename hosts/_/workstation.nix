f099484bd9268c3c0364b34b58e9099d7ae6988f
gautaz <11046232+gautaz@users.noreply.github.com>
2023-02-06 19:24:59 +0100

    add automated screen brightness management

--- hosts/_/workstation.nix
+++ hosts/_/workstation.nix
@@ -4,7 +4,9 @@ _: {
     systemd-boot.enable = true;
     efi.canTouchEfiVariables = true;
   };
+  location.provider = "geoclue2";
   networking.networkmanager.enable = true;
+  services.clight.enable = true;
   services.fwupd.enable = true;
   services.sshd.enable = true;
   time.timeZone = "Europe/Paris";
