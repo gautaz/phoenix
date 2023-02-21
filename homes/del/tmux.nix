97ad11eb2ad977bca9c650636301fb31423f2ecf
gautaz <11046232+gautaz@users.noreply.github.com>
2023-02-21 22:48:40 +0100

    tmux settings to comply with neovim needs

--- homes/del/tmux.nix
+++ homes/del/tmux.nix
@@ -6,9 +6,12 @@ _: {
       unbind-key C-a
       bind-key a send-prefix
       bind-key C-a last-window
+      set-option -sa terminal-overrides ',alacritty:RGB'
+      set-option -sg escape-time 10
     '';
     historyLimit = 10000;
     keyMode = "vi";
     prefix = "C-a";
+    terminal = "tmux-256color";
   };
 }
