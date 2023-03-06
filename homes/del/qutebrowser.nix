8056a46b71444a4e2799792afe19b5ec7dcf3f4f
gautaz <11046232+gautaz@users.noreply.github.com>
2023-03-06 22:40:07 +0100

    return to previous qutebrowser tab on remove

--- homes/del/qutebrowser.nix
+++ homes/del/qutebrowser.nix
@@ -4,6 +4,7 @@ _: {
     settings = {
       auto_save.session = true;
       editor.command = ["alacritty" "-e" "vim" "-f" "{file}" "-c" "normal {line}G{column0}l"];
+      tabs.select_on_remove = "prev";
     };
   };
 }
