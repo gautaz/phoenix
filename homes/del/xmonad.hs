a3d71c71b771a7756203fc2839e316254e458df5
gautaz <11046232+gautaz@users.noreply.github.com>
2023-03-09 19:02:23 +0100

    use ASD instead of WER for screen selection
    Also fix screen selection by assigning keybindings based on the screen
    layout reported by xinerama.

--- homes/del/xmonad.hs
+++ homes/del/xmonad.hs
@@ -1,17 +1,21 @@
+import Graphics.X11.Types (xK_a, xK_d, xK_e, xK_r, xK_s, xK_w)
 import XMonad
-  ( borderWidth
+  ( (.|.)
+  , borderWidth
   , def
   , focusFollowsMouse
   , mod4Mask
   , modMask
+  , shiftMask
   , spawn
   , terminal
   , xmonad
   )
+import XMonad.Actions.PhysicalScreens (sendToScreen, viewScreen)
 import XMonad.Hooks.EwmhDesktops (ewmh, ewmhFullscreen)
 import XMonad.Hooks.StatusBar (defToggleStrutsKey, statusBarProp, withEasySB)
 import XMonad.Hooks.StatusBar.PP (xmobarPP)
-import XMonad.Util.EZConfig (additionalKeysP)
+import XMonad.Util.EZConfig (additionalKeys, additionalKeysP, removeKeys)
 
 main :: IO ()
 main =
@@ -19,22 +23,29 @@ main =
 
 mySB = statusBarProp "xmobar" (pure xmobarPP)
 
+modm = mod4Mask
+
 myConfig =
   def
     { borderWidth = 2
     , focusFollowsMouse = False
-    , modMask = mod4Mask
+    , modMask = modm
     , terminal = "alacritty"
     } `additionalKeysP`
   [ ( "M-p"
     , spawn "rofi -show combi -modes combi -combi-modes 'window,drun,run'")
   , ( "M-o"
     , spawn "rofi -modi file-browser-extended -show file-browser-extended")
-  , ("M-s", spawn "systemctl suspend")
+  , ("M-S-<Escape>", spawn "systemctl suspend")
   , ("M-<Escape>", spawn "xsecurelock")
   , ("<XF86AudioLowerVolume>", spawn "outctl audio-down")
   , ("<XF86AudioMute>", spawn "outctl audio-toggle")
   , ("<XF86AudioRaiseVolume>", spawn "outctl audio-up")
   , ("<XF86MonBrightnessDown>", spawn "outctl brightness-down")
   , ("<XF86MonBrightnessUp>", spawn "outctl brightness-up")
-  ]
+  ] `additionalKeys`
+  [ ((modm .|. mask, key), action screenId)
+  | (key, screenId) <- zip [xK_a, xK_s, xK_d] [0 ..]
+  , (action, mask) <- [(viewScreen def, 0), (sendToScreen def, shiftMask)]
+  ] `removeKeys`
+  [(modm .|. mask, key) | mask <- [0, shiftMask], key <- [xK_w, xK_e, xK_r]]
