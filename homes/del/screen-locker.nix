2092ee64046d6dc9e0c7577de86a86772784d30f
gautaz <11046232+gautaz@users.noreply.github.com>
2023-03-16 09:13:24 +0100

    fix xsecurelock dimming and configuration

--- homes/del/screen-locker.nix
+++ homes/del/screen-locker.nix
@@ -1,12 +1,33 @@
-{pkgs, ...}: {
-  home.packages = with pkgs; [
+{pkgs, ...}:
+with pkgs; let
+  xdimmer = writeShellApplication {
+    name = "xdimmer";
+    text = ''
+      export XSECURELOCK_DIM_ALPHA=1
+      "${xsecurelock}/libexec/xsecurelock/dimmer" "$@"
+    '';
+  };
+  xlocker = writeShellApplication {
+    name = "xlocker";
+    text = ''
+      export XSECURELOCK_PASSWORD_PROMPT=time_hex
+      "${xsecurelock}/bin/xsecurelock" "$@"
+    '';
+  };
+in {
+  home.packages = [
+    xlocker
     xsecurelock
   ];
 
   services.screen-locker = {
     enable = true;
     inactiveInterval = 3;
-    lockCmd = "${pkgs.xsecurelock}/bin/xsecurelock";
-    xss-lock.extraOptions = ["-n" "${pkgs.xsecurelock}/libexec/xsecurelock/dimmer" "-l"];
+    lockCmd = "${xlocker}/bin/xlocker";
+    xautolock.enable = false;
+    xss-lock = {
+      extraOptions = ["-n" "${xdimmer}/bin/xdimmer" "-l"];
+      screensaverCycle = 5; # https://github.com/nix-community/home-manager/issues/2852
+    };
   };
 }
