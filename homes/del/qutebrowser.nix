67b9ddb43de0af251901c069a3a8d4ce123c4dbe
gautaz <11046232+gautaz@users.noreply.github.com>
2023-03-19 16:57:06 +0100

    ensure qutebrowser GUI choices are kept

--- homes/del/qutebrowser.nix
+++ homes/del/qutebrowser.nix
@@ -1,6 +1,7 @@
 _: {
   programs.qutebrowser = {
     enable = true;
+    loadAutoconfig = true;
     settings = {
       auto_save.session = true;
       editor.command = ["alacritty" "-e" "vim" "-f" "{file}" "-c" "normal {line}G{column0}l"];
