c8981264b3e443f162bae20c2ea169e5742548e3
gautaz <11046232+gautaz@users.noreply.github.com>
2023-04-06 07:25:36 +0200

    easier to remind terminal split keys

--- homes/del/tmux.nix
+++ homes/del/tmux.nix
@@ -6,6 +6,10 @@ _: {
       unbind-key C-a
       bind-key a send-prefix
       bind-key C-a last-window
+      unbind \"
+      unbind %
+      bind-key _ split-window -v
+      bind-key | split-window -h
       set-option -sa terminal-overrides ',alacritty:RGB'
       set-option -sg escape-time 10
     '';
