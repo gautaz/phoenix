ddc19e34bf4528857f9f5e1f0e61a041874685ee
gautaz <11046232+gautaz@users.noreply.github.com>
2023-04-30 15:21:17 +0200

    fix fzf preview issues

--- homes/del/fzf.nix
+++ homes/del/fzf.nix
@@ -1,29 +1,46 @@
-{pkgs, ...}:
+{
+  lib,
+  pkgs,
+  ...
+}:
 with pkgs; let
-  fzfCommand = "${fd}/bin/fd --follow --exclude \".git\"";
+  findCommand = "${fd}/bin/fd --follow --exclude \".git\"";
+  fsPreviewOption = lib.strings.concatStringsSep " " fsPreviewOptionList;
+  fsPreviewOptionList = ["--preview 'interview {}'"];
 in {
   home.packages = [
+    dnsutils
     fd
   ];
 
   programs = {
     bash.initExtra = ''
       _fzf_compgen_path() {
-        ${fzfCommand} . "$1"
+        ${findCommand} . "$1"
       }
       export _fzf_compgen_path
 
       _fzf_compgen_dir() {
-        ${fzfCommand} --type d . "$1"
+        ${findCommand} --type d . "$1"
       }
       export _fzf_compgen_dir
+
+      _fzf_comprun() {
+        local command="$1"
+        shift
+        case "$command" in
+          export|unset) fzf --preview "eval 'echo \$'{}" "$@" ;;
+          ssh) fzf --preview '${dnsutils}/bin/dig {}' "$@" ;;
+          *) fzf ${fsPreviewOption} "$@" ;;
+        esac
+      }
+      export _fzf_comprun
     '';
 
     fzf = {
-      defaultCommand = "${fzfCommand}";
-      defaultOptions = [
-        "--preview 'interview {}'"
-      ];
+      changeDirWidgetOptions = fsPreviewOptionList;
+      defaultCommand = "${findCommand}";
+      fileWidgetOptions = fsPreviewOptionList;
       enable = true;
       enableBashIntegration = true;
       tmux.enableShellIntegration = true;
