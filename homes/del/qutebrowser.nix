7d01e16c0e1fb775573be7ad6a7f4bf330304647
gautaz <11046232+gautaz@users.noreply.github.com>
2023-05-18 11:39:30 +0200

    qutebrowser: use bleeding edge rev

--- homes/del/qutebrowser.nix
+++ homes/del/qutebrowser.nix
@@ -1,10 +1,17 @@
-_: {
+{pkgs, ...}: {
   programs.qutebrowser = {
     enable = true;
     extraConfig = ''
       config.unbind('d', mode='normal')
     '';
     loadAutoconfig = true;
+    package = pkgs.qutebrowser-qt6.overrideAttrs (_: {
+      src = pkgs.fetchgit {
+        url = "https://github.com/qutebrowser/qutebrowser.git";
+        rev = "04c64b7430bbb1cee64927541da8b8fae75c92e3";
+        sha256 = "sha256-xUjesizJ73JBqU151uJo3o5O2mMU/gyRlcreg7nXlmI=";
+      };
+    });
     settings = {
       auto_save.session = true;
       colors.webpage = {
