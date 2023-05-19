7165f126e2b7329ae9a6be3d1277ab4b4f2b02c8
gautaz <11046232+gautaz@users.noreply.github.com>
2023-05-19 18:43:45 +0200

    qutebrowser: additional settings

--- homes/del/qutebrowser.nix
+++ homes/del/qutebrowser.nix
@@ -1,10 +1,19 @@
 {pkgs, ...}: {
   programs.qutebrowser = {
     enable = true;
+
     extraConfig = ''
       config.unbind('d', mode='normal')
     '';
+
+    keyBindings = {
+      normal = {
+        D = "tab-close";
+      };
+    };
+
     loadAutoconfig = true;
+
     package = pkgs.qutebrowser-qt6.overrideAttrs (_: {
       src = pkgs.fetchgit {
         url = "https://github.com/qutebrowser/qutebrowser.git";
@@ -12,15 +21,29 @@
         sha256 = "sha256-xUjesizJ73JBqU151uJo3o5O2mMU/gyRlcreg7nXlmI=";
       };
     });
+
+    searchEngines = {
+      DEFAULT = "https://duckduckgo.com/?q={}";
+      go = "https://www.google.com/search?hl=en&q={}";
+      gh = "https://github.com/search?q={}";
+      nh = "https://mipmip.github.io/home-manager-option-search/?{}";
+      no = "https://search.nixos.org/options?channel=unstable&query={}";
+      np = "https://search.nixos.org/packages?channel=unstable&query={}";
+    };
+
     settings = {
       auto_save.session = true;
       colors.webpage = {
         preferred_color_scheme = "light";
       };
       editor.command = ["alacritty" "-e" "vim" "-f" "{file}" "-c" "normal {line}G{column0}l"];
-      # The following is a work around to avoid new tabs cluttering the pinned tab "group":
-      # https://github.com/qutebrowser/qutebrowser/issues/2898#issuecomment-322009386
-      tabs.new_position.related = "last";
+      tabs = {
+        background = false;
+        mousewheel_switching = false;
+        # The following is a work around to avoid new tabs cluttering the pinned tab "group":
+        # https://github.com/qutebrowser/qutebrowser/issues/2898#issuecomment-322009386
+        new_position.related = "last";
+      };
     };
   };
 }
