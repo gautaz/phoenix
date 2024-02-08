d9ca04e97bc070ddb558bbd2ceb1d24fdd138aba
gautaz <11046232+gautaz@users.noreply.github.com>
2024-02-08 16:22:24 +0100

    nixpkgs overlay test

--- hosts/dante/default.nix
+++ hosts/dante/default.nix
@@ -1,10 +1,37 @@
 {pkgs, ...}: {
   imports = [../_/development.nix ../_/hw/dell-p117f.nix ../_/workstation.nix];
   networking.hostName = "dante";
+  nixpkgs.overlays = [
+    (final: prev: let
+      config-toml = prev.pkgs.writeText "config.toml" ''
+        disable-require = false
+        #swarm-resource = "DOCKER_RESOURCE_GPU"
+
+        [nvidia-container-cli]
+        #root = "/run/nvidia/driver"
+        #path = "/usr/bin/nvidia-container-cli"
+        environment = []
+        #debug = "/var/log/nvidia-container-runtime-hook.log"
+        ldcache = "/tmp/ld.so.cache"
+        load-kmods = true
+        no-cgroups = true
+        #user = "root:video"
+        ldconfig = "@@glibcbin@/bin/ldconfig"
+      '';
+    in {
+      nvidia-docker = prev.pkgs.mkNvidiaContainerPkg {
+        name = "nvidia-docker";
+        containerRuntimePath = "runc";
+        configTemplate = config-toml;
+        additionalPaths = [(prev.pkgs.callPackage <nixpkgs/pkgs/applications/virtualization/nvidia-docker> {})];
+      };
+    })
+  ];
   virtualisation.multipass.enable = true;
   virtualisation.docker.enableNvidia = true;
   virtualisation.docker.rootless.daemon.settings = {
     default-runtime = "nvidia";
     runtimes.nvidia.path = "${pkgs.nvidia-docker}/bin/nvidia-container-runtime";
+    exec-opts = ["native.cgroupdriver=cgroupfs"];
   };
 }
