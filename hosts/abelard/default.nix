7f6abc65648d3a377038b15fdffecd90992b5d1d
gautaz <11046232+gautaz@users.noreply.github.com>
2024-03-20 09:08:13 +0100

    vm tooling

--- hosts/abelard/default.nix
+++ hosts/abelard/default.nix
@@ -2,5 +2,7 @@ _: {
   # dell P165G
   imports = [../_/development.nix ../_/hw/dell-laptop.nix ../_/workstation.nix];
   networking.hostName = "abelard";
+  users.users.del.extraGroups = ["vboxusers"];
   virtualisation.multipass.enable = true;
+  virtualisation.virtualbox.host.enable = true;
 }
