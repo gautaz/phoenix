ca05c0cf58daeffdc34685e8904260be414ec1ee
gautaz <11046232+gautaz@users.noreply.github.com>
2024-03-24 12:45:33 +0100

    update flake dependencies

--- hosts/_/workstation.nix
+++ hosts/_/workstation.nix
@@ -7,6 +7,11 @@ _: {
   location.provider = "geoclue2";
   services = {
     avahi.enable = true;
+
+    # https://github.com/NixOS/nixpkgs/issues/298150#issuecomment-2016658808
+    # https://nixpk.gs/pr-tracker.html?pr=298491
+    fprintd.enable = false;
+
     fwupd.enable = true;
     sshd.enable = true;
     udisks2.enable = true;
