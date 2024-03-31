7f31ea48920a0d3afcb6b08bc4b8278460d93198
gautaz <11046232+gautaz@users.noreply.github.com>
2024-03-31 19:55:11 +0200

    networkmanager: disable wireless when wired

--- hosts/_/networkmanager.nix
+++ hosts/_/networkmanager.nix
@@ -1,4 +1,32 @@
-_: {
-  networking.networkmanager.enable = true;
+{pkgs, ...}: {
+  networking.networkmanager = {
+    dispatcherScripts = [
+      {
+        # based on https://askubuntu.com/a/112969
+        source = pkgs.writeText "disable-wireless-when-wired" ''
+          log() { logger -p user.info -t "disable-wireless-when-wired[$$]" "$*"; }
+          IFACE=$1
+          ACTION=$2
+
+          case $IFACE in
+            eth*|usb*|en*)
+              case $ACTION in
+                up)
+                  log "disabling wifi radio"
+                  ${pkgs.networkmanager}/bin/nmcli radio wifi off
+                  ;;
+                down)
+                  log "enabling wifi radio"
+                  ${pkgs.networkmanager}/bin/nmcli radio wifi on
+                  ;;
+              esac
+              ;;
+          esac
+        '';
+        type = "basic";
+      }
+    ];
+    enable = true;
+  };
   users.users.del.extraGroups = ["networkmanager"];
 }
