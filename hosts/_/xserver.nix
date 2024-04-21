c470e34004a0ff124ca4c0a0b65c73bbcbde34fd
gautaz <11046232+gautaz@users.noreply.github.com>
2024-04-21 23:13:10 +0200

    fix display manager settings

--- hosts/_/xserver.nix
+++ hosts/_/xserver.nix
@@ -1,16 +1,20 @@
 _: {
-  services.xserver = {
+  services = {
     displayManager = {
+      enable = true;
       defaultSession = "none+xmonad";
-      lightdm.enable = true;
     };
-    enable = true;
-    libinput.touchpad = {
-      clickMethod = "clickfinger";
-      naturalScrolling = true;
-      tapping = false;
+
+    xserver = {
+      displayManager.lightdm.enable = true;
+      enable = true;
+      libinput.touchpad = {
+        clickMethod = "clickfinger";
+        naturalScrolling = true;
+        tapping = false;
+      };
+      windowManager.xmonad.enable = true;
+      xkb.options = "compose:ralt";
     };
-    windowManager.xmonad.enable = true;
-    xkb.options = "compose:ralt";
   };
 }
