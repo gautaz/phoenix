b24284840d8fb3b6ce070cd1693722215c5a733b
gautaz <11046232+gautaz@users.noreply.github.com>
2024-05-13 09:24:21 +0200

    fix nm-online issue on rebuild switch

--- hosts/_/networkmanager.nix
+++ hosts/_/networkmanager.nix
@@ -28,5 +28,13 @@
     ];
     enable = true;
   };
+
+  # See https://github.com/NixOS/nixpkgs/issues/180175#issuecomment-1658731959
+  systemd.services.NetworkManager-wait-online = {
+    serviceConfig = {
+      ExecStart = ["" "${pkgs.networkmanager}/bin/nm-online -q"];
+    };
+  };
+
   users.users.del.extraGroups = ["networkmanager"];
 }
