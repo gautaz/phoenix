c7b560985ad2448df25082d020956cd428d3d366
gautaz <11046232+gautaz@users.noreply.github.com>
2024-06-30 10:00:19 +0200

    add zaproxy

--- homes/del/work/default.nix
+++ homes/del/work/default.nix
@@ -4,8 +4,25 @@
       aws-sam-cli
       awscli2
       docker-compose
+      firefox # needed by zap for manual explore
       jira-cli-go
       mark
+      zap
     ];
+
+    sessionVariables = {
+      BROWSER = "qutebrowser";
+    };
+  };
+
+  xdg.mimeApps = {
+    defaultApplications = {
+      "text/html" = "org.qutebrowser.qutebrowser.desktop";
+      "x-scheme-handler/http" = "org.qutebrowser.qutebrowser.desktop";
+      "x-scheme-handler/https" = "org.qutebrowser.qutebrowser.desktop";
+      "x-scheme-handler/about" = "org.qutebrowser.qutebrowser.desktop";
+      "x-scheme-handler/unknown" = "org.qutebrowser.qutebrowser.desktop";
+    };
+    enable = true;
   };
 }
