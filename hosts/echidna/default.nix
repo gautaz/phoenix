269856c36838260f3a2c2b9d56e40aacc1aa9821
gautaz <11046232+gautaz@users.noreply.github.com>
2024-08-05 08:09:35 +0200

    kernel debug

--- hosts/echidna/default.nix
+++ hosts/echidna/default.nix
@@ -1,4 +1,13 @@
 _: {
+  boot.kernelPatches = [
+    {
+      name = "kernel-debug";
+      patch = null;
+      extraConfig = ''
+        GDB_SCRIPTS y
+      '';
+    }
+  ];
   networking.hostName = "echidna";
   services.openssh.enable = true;
   time.timeZone = "Europe/Paris";
