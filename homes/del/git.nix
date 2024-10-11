0702f527173447d54a6373b29406c45517823f16
gautaz <11046232+gautaz@users.noreply.github.com>
2024-10-11 08:11:49 +0200

    remove pass-git-helper override as issue 387 is fixed

--- homes/del/git.nix
+++ homes/del/git.nix
@@ -4,18 +4,6 @@
   ...
 }: let
   mkSymlink = config.lib.file.mkOutOfStoreSymlink;
-
-  # Temporary fix to https://github.com/languitar/pass-git-helper/issues/387
-  pass-git-helper = pkgs.pass-git-helper.overrideAttrs (finalAttrs: previousAttrs: rec {
-    name = "pass-git-helper-${version}";
-    version = "1.4.0";
-    src = pkgs.fetchFromGitHub {
-      owner = "languitar";
-      repo = "pass-git-helper";
-      rev = "refs/tags/v${version}";
-      sha256 = "sha256-wkayj7SvT3SOM+rol17+8LQJR/YXSC6I+iKbHRUbdZc=";
-    };
-  });
 in {
   home = {
     file.".config/pass-git-helper/git-pass-mapping.ini".source = mkSymlink "/run/secrets/git/passGitHelper";
@@ -29,7 +17,7 @@ in {
     };
     enable = true;
     extraConfig = {
-      credential.helper = "${pass-git-helper}/bin/pass-git-helper";
+      credential.helper = "${pkgs.pass-git-helper}/bin/pass-git-helper";
       init = {
         defaultBranch = "main";
       };
