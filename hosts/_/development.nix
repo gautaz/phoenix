b47d533c59d35fae5c9be41066529d6f0f7e7103
gautaz <11046232+gautaz@users.noreply.github.com>
2024-11-29 09:44:40 +0100

    ensure wireshark does not require root permissions

--- hosts/_/development.nix
+++ hosts/_/development.nix
@@ -1,5 +1,8 @@
-_: {
+{pkgs, ...}: {
   imports = [./docker.nix];
-  programs.wireshark.enable = true;
+  programs.wireshark = {
+    enable = true;
+    package = pkgs.wireshark;
+  };
   users.users.del.extraGroups = ["wireshark"];
 }
