cf23cd3e139de1f351e218cc60cc5b35f718fa9a
gautaz <11046232+gautaz@users.noreply.github.com>
2025-10-16 18:10:45 +0200

    fix home-manager warning about ssh default config being deprecated

--- homes/del/ssh.nix
+++ homes/del/ssh.nix
@@ -1,6 +1,19 @@
 _: {
   programs.ssh = {
     enable = true;
+    enableDefaultConfig = false;
     includes = ["/run/secrets/ssh/config/*"];
+    matchBlocks."*" = {
+      forwardAgent = false;
+      addKeysToAgent = "no";
+      compression = false;
+      serverAliveInterval = 0;
+      serverAliveCountMax = 3;
+      hashKnownHosts = false;
+      userKnownHostsFile = "~/.ssh/known_hosts";
+      controlMaster = "no";
+      controlPath = "~/.ssh/master-%r@%n:%p";
+      controlPersist = "no";
+    };
   };
 }
