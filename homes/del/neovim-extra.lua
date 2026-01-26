db61fb685997201679b9025be7c838e60832112c
gautaz <11046232+gautaz@users.noreply.github.com>
2026-01-26 08:52:12 +0100

    fix diagnostic goto_prev/goto_next deprecation

--- homes/del/neovim-extra.lua
+++ homes/del/neovim-extra.lua
@@ -8,17 +8,17 @@ vim.diagnostic.config({
 
 vim.g.mapleader = " "
 
-vim.keymap.set({'n', 'x'}, 'ga', '<Plug>(EasyAlign)')
-
-vim.keymap.set("n", "<Leader>d", vim.diagnostic.open_float)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-
 vim.opt.cursorline = true
 vim.opt.diffopt:append "vertical"
 vim.opt.mouse = ""
 vim.opt.termguicolors = true
 vim.opt.wildmode = "longest:full,full"
+
+vim.keymap.set({'n', 'x'}, 'ga', '<Plug>(EasyAlign)')
+
+vim.keymap.set("n", "<Leader>d", vim.diagnostic.open_float)
+vim.keymap.set('n', '[d', function() vim.diagnostic.jump({ count = -1, float = true }) end)
+vim.keymap.set('n', ']d', function() vim.diagnostic.jump({ count = 1, float = true }) end)
 
 vim.api.nvim_create_autocmd("LspAttach", {
 	group = vim.api.nvim_create_augroup("UserLspConfig", {}),
