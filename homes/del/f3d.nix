9951f082d3d4b72a2f33b95ee67e95010b1ff4b6
gautaz <11046232+gautaz@users.noreply.github.com>
2026-02-08 16:35:25 +0100

    remove useless mkSymlink for f3d conf

--- homes/del/f3d.nix
+++ homes/del/f3d.nix
@@ -1,13 +1,10 @@
 {
-  config,
   pkgs,
   ...
-}: let
-  mkSymlink = config.lib.file.mkOutOfStoreSymlink;
-in {
+}: {
   home = {
     file = {
-      ".config/f3d/config.json".source = mkSymlink ./f3d.json;
+      ".config/f3d/config.json".source = ./f3d.json;
     };
     packages = [
       pkgs.f3d
