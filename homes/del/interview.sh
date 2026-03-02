8fef3dcff1d820126270d7409ef9436b8aa656f6
gautaz <11046232+gautaz@users.noreply.github.com>
2026-03-02 11:51:08 +0100

    fix interview script usage without fzf

--- homes/del/interview.sh
+++ homes/del/interview.sh
@@ -1,14 +1,17 @@
 # shellcheck source=/dev/null
 FILETYPE="$(file --brief --mime "$1")"
+read -r LINES COLUMNS <<< "$(stty size 2>/dev/null || echo '25 80')"
+CURRENT_COLUMNS="${FZF_PREVIEW_COLUMNS:-$COLUMNS}"
+CURRENT_LINES="${FZF_PREVIEW_LINES:-$LINES}"
 case "$FILETYPE" in
 	application/pdf\;*)
 		TMPDIR="$(mktemp -d)"
 		pdftoppm -singlefile "$1" "$TMPDIR/preview"
-		chafa --view-size="${FZF_PREVIEW_COLUMNS}x${FZF_PREVIEW_LINES}" "$TMPDIR/preview.ppm"
+		chafa --view-size="${CURRENT_COLUMNS}x${CURRENT_LINES}" "$TMPDIR/preview.ppm"
 		rm -rf "$TMPDIR"
 		;;
 	image/*)
-		chafa --view-size="${FZF_PREVIEW_COLUMNS}x${FZF_PREVIEW_LINES}" "$1"
+		chafa --view-size="${CURRENT_COLUMNS}x${CURRENT_LINES}" "$1"
 		;;
 	inode/directory\;*)
 		tree -C -d -L 2 "$1"
@@ -22,7 +25,7 @@ case "$FILETYPE" in
 	video/*)
 		TMPDIR="$(mktemp -d)"
 		mpv --terminal=no --vo=image --vo-image-format=png --vo-image-outdir="$TMPDIR" --frames=1 --start=50% --no-audio "$1"
-		chafa --view-size="${FZF_PREVIEW_COLUMNS}x${FZF_PREVIEW_LINES}" "$TMPDIR/00000001.png"
+		chafa --view-size="${CURRENT_COLUMNS}x${CURRENT_LINES}" "$TMPDIR/00000001.png"
 		rm -rf "$TMPDIR"
 		;;
 	*)
