3b6f480d0a9313669ea6fb11d087043be1cfbdef
gautaz <11046232+gautaz@users.noreply.github.com>
2026-05-03 14:56:07 +0200

    start podman socket on login

--- homes/del/containers.nix
+++ homes/del/containers.nix
@@ -1,4 +1,8 @@
-{pkgs, ...}: {
+{
+  lib,
+  pkgs,
+  ...
+}: {
   home = {
     file.".local/bin/docker".source = "${pkgs.podman}/bin/podman";
     packages = with pkgs; [
@@ -16,4 +20,11 @@
       engine.compose_warning_logs = false;
     };
   };
+  # https://github.com/nix-community/home-manager/issues/9114
+  home.activation.enablePodmanSocket = lib.hm.dag.entryAfter ["writeBoundary"] ''
+    $DRY_RUN_CMD mkdir -p "$HOME/.config/systemd/user/sockets.target.wants"
+    $DRY_RUN_CMD ln -sfv "$HOME/.nix-profile/share/systemd/user/podman.socket" \
+      "$HOME/.config/systemd/user/sockets.target.wants/podman.socket"
+    $DRY_RUN_CMD "${pkgs.systemd}/bin/systemctl" --user daemon-reload || true
+  '';
 }
