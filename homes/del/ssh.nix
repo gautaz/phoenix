c87d4ca50850bb2328df5bd94b96359ee5059159
gautaz <11046232+gautaz@users.noreply.github.com>
2026-05-22 16:30:15 +0200

    use settings instead of matchBlocks in programs.ssh

--- homes/del/ssh.nix
+++ homes/del/ssh.nix
@@ -3,17 +3,17 @@ _: {
     enable = true;
     enableDefaultConfig = false;
     includes = ["/run/secrets/ssh/config/*"];
-    matchBlocks."*" = {
-      forwardAgent = false;
-      addKeysToAgent = "no";
-      compression = false;
-      serverAliveInterval = 0;
-      serverAliveCountMax = 3;
-      hashKnownHosts = false;
-      userKnownHostsFile = "~/.ssh/known_hosts";
-      controlMaster = "no";
-      controlPath = "~/.ssh/master-%r@%n:%p";
-      controlPersist = "no";
+    settings."*" = {
+      ForwardAgent = false;
+      AddKeysToAgent = "no";
+      Compression = false;
+      ServerAliveInterval = 0;
+      ServerAliveCountMax = 3;
+      HashKnownHosts = false;
+      UserKnownHostsFile = "~/.ssh/known_hosts";
+      ControlMaster = "no";
+      ControlPath = "~/.ssh/master-%r@%n:%p";
+      ControlPersist = "no";
     };
   };
 }
