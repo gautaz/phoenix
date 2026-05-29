0bf6488c052f4b1fce2108e620c90761158152d0
gautaz <11046232+gautaz@users.noreply.github.com>
2026-05-29 15:13:35 +0200

    fix potential neovim-plugins import issue

--- homes/del/neovim-plugins/default.nix
+++ homes/del/neovim-plugins/default.nix
@@ -1,6 +1,6 @@
 {pkgs}:
 with pkgs.lib; (trivial.pipe (builtins.readDir ./.) [
-  (attrsets.filterAttrs (name: value: name != "default.nix" && value == "regular"))
+  (attrsets.filterAttrs (name: value: name != "default.nix" && value == "regular" && strings.hasSuffix ".nix" name))
   (attrsets.mapAttrs' (name: value:
     attrsets.nameValuePair (strings.removeSuffix ".nix" name) (import (./. + "/${name}") {
       inherit pkgs;
